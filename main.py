2dfdfcdf8018973827d5a2ad9a2ff2ee9d6f1805
Mike Bocek <mbocek@stanford.edu>
2017-02-03 21:42:56 -0800

    Minor syntax fixes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import profile
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,23 +8,27 @@ from objects import Camera, Sphere, Light
 height = 500
 width = 500
 
-c = Camera(np.array([0,0,-4]), np.array([0,0,1.0]), width, height, 30, 1)
-spheres = [Sphere(np.array([0,-2,5]), 1)]
+c = Camera(np.array([0,0,-4.0]), np.array([0,0,2.0]), width, height, 30, 2)
+spheres = [Sphere(np.array([0,0,5]), 1)]
 lights = [Light(np.array([1000,1000,-1000]))]
 
 pixels = np.zeros((height, width))
 
-for i in range(width):
-    for j in range(height):
-        v = c.vector_from_pixels(i, j)
-        for sphere in spheres:
-            p = sphere.intersection_point(c.pos, v)
-            if p is not None:
-                pixels[j, i] = sphere.color(p, lights)
-                break
+def main():
+    for i in range(width):
+        for j in range(height):
+            v = c.vector_from_pixels(i, j)
+            for sphere in spheres:
+                p = sphere.intersection_point(c.pos, v)
+                if p is not None:
+                    pixels[j, i] = sphere.color(p, lights)
+                    break
 
 
-plt.imshow(pixels, cmap='Greys')
-plt.axis('off')
+    plt.imshow(pixels, cmap='Greys')
+    plt.axis('off')
 
-plt.savefig('sphere.png')
+    plt.savefig('sphere.png')
+
+if __name__ == '__main__':
+    main()
