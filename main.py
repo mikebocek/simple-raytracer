Fix color normalization

main.py
@@ -11,16 +11,22 @@ width = 640
 height = 480
 
 c = Camera(np.array([0,0,-4.0]), np.array([0,0,2.0]), height, width, 30, 2)
-spheres = [Sphere(np.array([0,0,5]), 1)]
-lights = [PointLight(np.array([1000,1000,-1000]))]
+spheres = [Sphere(np.array([0,0,5]), 1), Sphere(np.array([4,0,10]), 1)]
+lights = [PointLight(np.array([-1000,-1000,1000]))]
 
 pixels = np.zeros((height, width, 3))
 
 material = LambertShader()
-texture = Color([0.2,0.5,0.3])
-bg_color = Color([0,0,1])
+texture = Color([0.0,0.3,0.3])
+bg_color = Color([0,0,0])
+
+def truncate(vector, minimum, maximum):
+    min_truncated = np.max(np.array([vector, np.full(vector.shape, minimum, dtype=np.int64)]), axis=0)
+    return np.min(np.array([min_truncated, np.full(vector.shape, maximum, dtype=np.int64)]), axis=0)
+
 
 def main():
+    print(texture.color[0:3])
     for i in range(height):
         for j in range(width):
             v = c.vector_from_pixels(i, j)
@@ -28,10 +34,10 @@ def main():
                 p = sphere.intersection_point(c.pos, v)
                 if p is not None:
                     n = sphere.normal_vector(p)
-                    pixels[i, j, :] = (255 * material.shade(p, n, texture, lights).color[0:3]).astype(np.int8)
+                    pixels[i, j, :] = truncate(255 * material.shade(p, n, texture, lights).color[0:3], 0, 255)
                     break
                 else:
-                    pixels[i, j, :] = (255*bg_color.color[0:3]).astype(np.int8)
+                    pixels[i, j, :] = truncate(255*bg_color.color[0:3], 0 ,255)
 
     np.save('test.npy', pixels)
     imsave('Sphere.png', pixels)

shaders.py
@@ -9,5 +9,5 @@ class LambertShader():
         for light_source in light_sources[:1]:
             normed_light = (intersection_point - light_source.point)
             normed_light /= np.linalg.norm(normed_light)
-            color.color = np.array([0,0.3,0]) * (normal_vector @ normed_light)
+            color = Color(material_color.color[0:3] * (normal_vector @ normed_light))
         return color