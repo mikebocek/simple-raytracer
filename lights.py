c9e9cc315747a1bc0d8789aaafe7d18b03d677ec
Mike Bocek <mike.bocek@gmail.com>
2017-02-08 09:36:41 -0800

    Add support for colored lights

--- lights.py
+++ lights.py
@@ -1,3 +1,6 @@
+from color import Color
+
 class PointLight():
-    def __init__(self, point):
+    def __init__(self, point, color = Color([1,1,1])):
         self.point = point
+        self.color = color
--- shaders.py
+++ shaders.py
@@ -7,4 +7,4 @@ class LambertShader():
     def compute_color(intersection_point, normal_vector, material_color, light_source):
         normed_light = (light_source.point - intersection_point)
         normed_light /= np.linalg.norm(normed_light)
-        return Color(material_color.color[0:3] * (normal_vector @ normed_light))
+        return Color(material_color.color[0:3] * light_source.color.color[0:3] * (normal_vector @ normed_light))
